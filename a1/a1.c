8aad4efaa0c943d312c6a92f4fdf85b3b314915f
daniboar <daniboar@yahoo.com>
2023-04-03 20:20:16 +0300

    Second try

--- a1/a1.c
+++ a1/a1.c
@@ -7,33 +7,89 @@
 #include <unistd.h>
 
 
-void cerinta_2(const char *path, const char *filename)
+void list_it(const char *path, const char *name, const int size)
 {
     DIR *dir = NULL;
     struct dirent *entry = NULL;
     char fullPath[512];
-    char f[512];
+    struct stat statbuf;
 
     dir = opendir(path);
-    if(dir == NULL)
-    {
-        printf("ERROR\ninvalid directory path\n");
+    if(dir == NULL){
+        printf("ERROR\ninvalid directory path");
+        return;
+    }
+    printf("SUCCESS\n");
+    if(name != NULL && size == 0){
+    while((entry = readdir(dir)) != NULL){
+        if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
+            if(strncmp(entry->d_name, name, strlen(name)) == 0){
+                snprintf(fullPath, 512, "%s/%s", path, entry->d_name);
+                    if(lstat(fullPath, &statbuf) == 0){
+                        printf("%s\n", fullPath);
+                    }
+                }
+            }
+        }
+    }
+    else if(name == NULL && size != 0){
+
+    }
+    else{
+        while((entry = readdir(dir)) != NULL){
+        if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
+                snprintf(fullPath, 512, "%s/%s", path, entry->d_name);
+                    if(lstat(fullPath, &statbuf) == 0){
+                        printf("%s\n", fullPath);
+                    }
+            }
+        }
+    }
+    closedir(dir);
+}
+
+void list_rec(const char *path, const char *name, const int size)
+{
+    DIR *dir = NULL;
+    struct dirent *entry = NULL;
+    char fullPath[512];
+    struct stat statbuf;
+
+    dir = opendir(path);
+    if(dir == NULL){
+        printf("ERROR\ninvalid directory path");
         return;
     }
 
-    while((entry = readdir(dir)) != NULL)
-    {
-        if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
-        {
-            snprintf(fullPath, 512, "%s/%s", path, entry->d_name);
-            FILE *file = fopen(entry->d_name, "r");
-            if(file == NULL)
-            {
-                printf("SUCCES\n");
-                return ;
-
+    if(name != NULL && size == 0){
+    while((entry = readdir(dir)) != NULL){
+        if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
+            if(strncmp(entry->d_name, name, strlen(name)) == 0){
+                snprintf(fullPath, 512, "%s/%s", path, entry->d_name);
+                if(lstat(fullPath, &statbuf) == 0){
+                    printf("%s\n", fullPath);
+                    if(S_ISDIR(statbuf.st_mode)){
+                        //list_rec(fullPath, 0, 0);
+                    }
+                }
             }
-            //closedir(dir);
+        } 
+    }
+    }
+    else if(name == NULL && size != 0){
+
+    }
+    else{
+        while((entry = readdir(dir)) != NULL){
+        if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
+                snprintf(fullPath, 512, "%s/%s", path, entry->d_name);
+                if(lstat(fullPath, &statbuf) == 0){
+                    printf("%s\n", fullPath);
+                    if(S_ISDIR(statbuf.st_mode)){
+                        list_rec(fullPath, 0, 0);
+                    }
+                }   
+            } 
         }
     }
     closedir(dir);
@@ -48,10 +104,42 @@ int main(int argc, char **argv)
         {
             printf("28180\n");
         }
-        else
-        {
-            cerinta_2(argv[1], argv[2]);
-        }
-    }
+        if(strcmp(argv[1], "list") == 0){
+            if(strstr(argv[2], "path=") - argv[2] == 0 ) //daca nu e recursiv si nu avem filtering_option
+                    list_it(argv[2]+5, 0, 0);
+            else if(strstr(argv[2], "name_starts_with=") != NULL){// daca nu e recursiv si avem "name starts with.."
+                    if(strstr(argv[3], "path=") - argv[3] == 0 ){
+                    list_it(argv[3]+5, argv[2]+17, 0);
+                }
+            }
+            else if(strstr(argv[2], "size_smaller=") != NULL){// daca nu e recursiv si avem "size smaller"
+                    if(strstr(argv[3], "path=") - argv[3] == 0 ){
+                    //list_it(argv[3]+5, 0, argv[2]+13);
+                }
+            }
+            if(strcmp(argv[2], "recursive") == 0){// daca vrem listare recursiva doar
+                    if(strstr(argv[3], "path=") - argv[3] == 0 ){
+                        printf("SUCCESS\n");
+                        list_rec(argv[3]+5, 0, 0);
+                    }
+                    else if(strstr(argv[3], "name_starts_with=") != NULL){// daca e recursiv si avem "name starts with.."
+                        if(strstr(argv[4], "path=") - argv[4] == 0 ){
+                            printf("SUCCESS\n");
+                            list_rec(argv[4]+5, argv[3]+17, 0);
+                        }
+                    }
+                    else if(strstr(argv[3], "size_smaller=") != NULL){// daca e recursiv si avem "name starts with.."
+                        if(strstr(argv[4], "path=") - argv[4] == 0 ){
+                            printf("SUCCESS\n");
+                            //list_rec(argv[4]+5, 0, argv[3]+17);
+                        }
+                    }
+            
+            }
+        
+        }  
+    } 
     return 0;
+    
 }
+   
